ef263529273e7c8bc8902a32837e581e3a72e333
Rohan Parmar <rohanxparmar@gmail.com>
2022-11-24 09:38:30 -0800

    Updates

--- List.cpp
+++ List.cpp
@@ -1,9 +1,9 @@
 /*
  * List.cpp
- * 
+ *
  * Class Description: List data collection ADT.
- *                    Based on the Hashing strategy and the open addressing 
- *                    collision resolution strategy called linear probing hashing.             
+ *                    Based on the Hashing strategy and the open addressing
+ *                    collision resolution strategy called linear probing hashing.
  * Class Invariant: Data collection with the following characteristics:
  *                  - Each element is unique (no duplicates).
  *
@@ -11,20 +11,20 @@
  * Date: Last modified: Nov. 2022
  */
 
-
 #include <iostream>
-#include <string.h>
+#include <string>
 
+#include "Member.h"
 #include "List.h"
 #include "ElementAlreadyExistsException.h"
 #include "ElementDoesNotExistException.h"
 #include "UnableToInsertException.h"
 
 // Constructor
-List::List(unsigned int (*hFcn)(string))
-    : hashFcn(hFcn), hashTable(nullptr), elementCount(0) 
+List::List(unsigned int (*hFcn)(Member))
+    : hashFcn(hFcn), hashTable(nullptr), elementCount(0)
 {
-    hashTable = new Member* [CAPACITY];
+    hashTable = new Member *[CAPACITY];
     for (int i = 0; i < CAPACITY; i++)
     {
         hashTable[i] = nullptr;
@@ -35,39 +35,38 @@ List::List(unsigned int (*hFcn)(string))
 // Description: Destruct a List object, releasing heap-allocated memory.
 List::~List()
 {
-   for (int i = 0; i < CAPACITY; i++)
-   {
+    for (int i = 0; i < CAPACITY; i++)
+    {
         if (hashTable != nullptr)
         {
             delete hashTable[i];
-            delete [] hashTable;
         }
-   }
+        delete[] hashTable;
+    }
 }
 
 // Description: Returns the total element count currently stored in List.
 // Postcondition: List remains unchanged.
 unsigned int List::getElementCount() const
 {
-    returns elementCount;
+    return elementCount;
 }
-
 
 // Description: Returns a pointer to the target element if found.
 // Postcondition: List remains unchanged.
 // Exception: Throws EmptyDataCollectionException if the List is empty.
 // Exception: Throws ElementDoesNotExistException if newElement is not found in the List.
-Member* List::search( Member & target ) const
+Member *List::search(Member &target) const
 {
-    int index = hashFcn(target); // compute hash index
+    int index = hashFcn(&target); // compute hash index
 
     if (isEmpty()) // list is empty
     {
         throw EmptyDataCollectionException("Data collection is empty.");
     }
-    else if (hashTable[index] == target) // index is the target key
+    else if (hashTable[index] == &target) // index is the target key
     {
-        return (&hashTable[index]);
+        return (hashTable[index]);
     }
     else // target key not found
     {
@@ -78,14 +77,14 @@ Member* List::search( Member & target ) const
         else // cell is not empty -> collision
         {
             int i = 0;
-            while(hashTable[index] != target)
+            while (hashTable[index] != &target)
             {
                 index = (index + i) % CAPACITY;
                 i++;
             }
         }
     }
-    return (&hashTable[index]);
+    return (hashTable[index]);
 }
 
 // Description: Prints all elements stored in the List (unsorted).
@@ -101,6 +100,7 @@ void List::printList() const
 ////////////////////////////// Helper functions ///////////////////////////
 
 // Description: returns true if list is empty, otherwise false
-bool List::isEmpty() {
+bool List::isEmpty()
+{
     return elementCount == 0;
 }
